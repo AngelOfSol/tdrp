457e283672967b91536d5b942ec0802986bb2901
AngelOfSol <julienckilian@gmail.com>
2014-04-16 23:47:29 -0400

    Library Commit + Blank Window

--- game/game/Source.cpp
+++ game/game/Source.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <SFML\System.hpp>
+#include <SFML\Graphics.hpp>
+#include <SFML\Window.hpp>
 
 int main()
 {
+	
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Test", sf::Style::Default, settings);
+
+	sf::Clock timer;
+
+	sf::Time minimiumFrameTime = sf::seconds(1.0f / 60.0f);
+	sf::Event event;
+	int elapsedTotal = 0;
+		sf::RenderStates rs;
+	while(window.isOpen())
+	{
+		 while (window.pollEvent(event))
+        {
+            // Close window : exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+			
+        }
+		
+        // Clear screen
+        window.clear(sf::Color(255, 255, 255, 255));
+		// Draw stuff
+        // Update the window
+        window.display();
+	}
 	return 0;
 }
